215c816ebc8b812964178db3a77c69a8966b0e03
AlyonaKileynikova <105370125+AlyonaKileynikova@users.noreply.github.com>
2022-10-16 18:27:32 +0300

    HW-07-JS
    task 1 complete. task 2 add

---
+++ js/01-gallery.js
@@ -0,0 +1,80 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+// const refs = {
+//   gallery: document.querySelector(".js-gallery"),
+//   image: document.createElement("img"),
+//   lightbox: document.querySelector(".lightbox"),
+//   btn: document.querySelector('[data-action="close-lightbox"]'),
+//   modal: document.querySelector(".lightbox__content"),
+//   lightbox__image: document.querySelector(".lightbox__image"),
+// };
+
+
+//   --- 1. Markup ------
+
+const galleryTemplate = ({ preview, original, description }) =>
+`<div class="gallery__item">
+<a class="gallery__link" href="${original}">
+    <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+    />
+</a>
+</div>`
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = galleryItems.map(galleryItem => galleryTemplate(galleryItem)).join('');
+galleryContainer.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+
+// ---2. Get Large Image 
+
+// function onPreviewImageClick(evt) {
+//     evt.preventDefault();
+
+//     const isImage = evt.target.classList.contains ('gallery__image');
+//     if (!isImage)  return;
+
+//     const selectedImage = evt.target;
+
+//     const instance = basicLightbox.create(`<div class="gallery__item"><img src="${selectedImage.dataset.source}" width="600"/></div>`)
+
+//     instance.show();
+// }
+
+galleryContainer.addEventListener('click', evt => {
+    evt.preventDefault();
+    if (evt.target.nodeName !== 'IMG') {
+        return
+    }
+
+    const selectedImage = evt.target.getAttribute('data-source');
+
+    const instance = basicLightbox.create(`<img src="${selectedImage}" width="600"/>`);
+    
+    instance.show();
+
+    galleryContainer.addEventListener('keydown', evt => {
+        if (evt.key === 'Escape') {
+            instance.close();
+        }
+    })
+})
+
+
+// galleryContainer.addEventListener('click', onImageClick);
+// function onImageClick(evt) {
+//     evt.pretendDefault();
+
+//     const isImage = evt.target.classList.contains('gallery');
+//     if (!isImage) { return };
+
+//     const activeImage = evt.target;
+// }
+
+
---
+++ js/02-lightbox.js
@@ -0,0 +1,78 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+
+const galleryTemplate = galleryItems
+  .map(
+    ({
+      preview,
+      original,
+      description,
+    }) => `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+  )
+  .join("");
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryTemplate);
+// galleryContainer.addEventListener("click", onItemClick);
+
+// function onItemClick(evt) {
+//   evt.preventDefault();
+// }
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  caption: true,
+  captionSelector: "img",
+  captionPosition: "bottom",
+  captionData: "alt",
+  captionDelay: 250,
+});
+
+// galleryContainer.addEventListener("click", (evt) => {
+//   evt.preventDefault();
+//   if (evt.target.nodeName !== "IMG") {
+//     return;
+//   }
+// });
+    
+    
+// const galleryTemplate = ({ preview, original, description }) =>
+//   `<a class="gallery__item" href="${original}">
+//   <img class="gallery__image" 
+//   src="${preview}" 
+//   alt="${description}"/>
+// </a>`;
+
+// const galleryContainer = document.querySelector(".gallery");
+// const galleryMarkup = galleryItems
+//   .map((galleryItem) => galleryTemplate(galleryItem))
+//   .join("");
+// galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+
+
+
+// galleryContainer.addEventListener("click", (evt) => {
+//   evt.preventDefault();
+//   if (evt.target.nodeName !== "IMG") {
+//     return;
+//   }
+
+  // const selectedImage = evt.target;
+
+  // const instance = simpleLightbox.create(
+  //   `<img src="${selectedImage}" width="600"/>`
+  // );
+
+  // instance.show();
+
+  // galleryContainer.addEventListener("keydown", (evt) => {
+  //   if (evt.key === "Escape") {
+  //     instance.close();
+  //   }
+  // });
+// });
